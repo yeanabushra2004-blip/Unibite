fc4aed6b545672cbf7215bace88178e7dfb79103
Bushra <yeanabushra2004@gmail.com>
2026-09-10 10:56:38 +0600

    user creat

---
+++ backend/server/model/user.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+});
+const User = mongoose.model('User', userSchema);
+module.exports = User;
