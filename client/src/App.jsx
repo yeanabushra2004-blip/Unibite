422ff063262f1a275ce22baf84b4e409d49a4ca8
Rafi <rafiulrafi55@gmail.com>
2026-09-22 21:45:14 +0600

    fixed filepath conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import Register from './pages/Register';
+import Register from './pages/register';
 
 function App() {
     return (
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Register.css";
+import "./register.css";
 
 function Register() {
 	const [formData, setFormData] = useState({
